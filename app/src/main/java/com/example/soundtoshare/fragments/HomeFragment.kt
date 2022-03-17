705351d7bc3f000f8d15355cc3aeb677c249719d
Michail Shagovitov <shiga2001@mail.ru>
2022-03-17 14:11:58 +0300

    Изменил констрейты и начал делать первоначальный экран

--- app/src/main/java/com/example/soundtoshare/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/soundtoshare/fragments/HomeFragment.kt
@@ -21,7 +21,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
 
 }
