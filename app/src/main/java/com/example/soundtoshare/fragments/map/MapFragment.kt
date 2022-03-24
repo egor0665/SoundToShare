f283c559001cbd363dc027910a715ffe5c86816b
HolyDoly123 <vladimirudakovhd@gmail.com>
2022-03-25 01:37:03 +0300

    Добавлены в кривом виде фрагмент карты и отображение геопозиции на ней

--- app/src/main/java/com/example/soundtoshare/fragments/map/MapFragment.kt
+++ app/src/main/java/com/example/soundtoshare/fragments/map/MapFragment.kt
@@ -1,17 +1,116 @@
 package com.example.soundtoshare.fragments.map
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.soundtoshare.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.OnSuccessListener
 
-class MapFragment : Fragment() {
 
+
+
+class MapFragment : Fragment(), GoogleMap.OnMyLocationButtonClickListener,
+    GoogleMap.OnMyLocationClickListener,
+    ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private var permissionDenied = false
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        map = googleMap
+        googleMap.setOnMyLocationButtonClickListener(this)
+        googleMap.setOnMyLocationClickListener(this)
+        enableMyLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if (!::map.isInitialized) return
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            map.isMyLocationEnabled = true
+            var longitude = 0.0
+            var latitude = 0.0
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener(requireActivity()
+                ) { location ->
+                    // Got last known location. In some rare situations this can be null.
+                    if (location != null) {
+                        longitude = location.longitude
+                        latitude = location.latitude
+                    }
+                }
+            map.moveCamera(CameraUpdateFactory.newLatLng(LatLng(latitude, longitude)))
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+        //if (isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+       //     enableMyLocation()
+       // } else {
+            // Permission was denied. Display an error message
+            // Display the missing permission error dialog when the fragments resume.
+      //      permissionDenied = true
+      //  }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError()
+            permissionDenied = false
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private fun showMissingPermissionError() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        //newInstance(true).show(mapFragment, "dialog")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
     //2
     companion object {
-
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
         fun newInstance(): MapFragment {
             return MapFragment()
         }
