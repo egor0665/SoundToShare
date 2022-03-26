60663bcbe6fc10cfd46a3320e8a65922b36efbf8
Dmitry <dvm101@yandex.ru>
2022-03-27 01:10:53 +0300

    Добавлена авторизация Spotify

--- app/src/main/java/com/example/soundtoshare/MainActivity.kt
+++ app/src/main/java/com/example/soundtoshare/MainActivity.kt
@@ -1,18 +1,35 @@
 package com.example.soundtoshare
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.soundtoshare.databinding.ActivityMainBinding
 import com.example.soundtoshare.fragments.home.HomeFragment
 import com.example.soundtoshare.fragments.map.MapFragment
 import com.example.soundtoshare.fragments.settings.SettingsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.spotify.sdk.android.auth.AuthorizationClient
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var token: String
+    internal lateinit var launcher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                token = AuthorizationClient.getResponse(result.resultCode, result.data).accessToken
+                Log.d("token", token)
+            }
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
