404572b086563bea5609cdc4084091ce691d7538
Dmitry <dvm101@yandex.ru>
2022-04-02 00:45:45 +0300

    Добавлена авторизация VK

--- app/src/main/java/com/example/soundtoshare/MainActivity.kt
+++ app/src/main/java/com/example/soundtoshare/MainActivity.kt
@@ -1,33 +1,39 @@
 package com.example.soundtoshare
 
-import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import com.example.soundtoshare.databinding.ActivityMainBinding
 import com.example.soundtoshare.fragments.home.HomeFragment
 import com.example.soundtoshare.fragments.map.MapFragment
 import com.example.soundtoshare.fragments.settings.SettingsFragment
+import com.example.soundtoshare.workers.VkWorker
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.spotify.sdk.android.auth.AuthorizationClient
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var token: String
-    internal lateinit var launcher: ActivityResultLauncher<Intent>
-
+    internal lateinit var authVkLauncher:  ActivityResultLauncher<Collection<VKScope>>
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
-        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-                result: ActivityResult ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                token = AuthorizationClient.getResponse(result.resultCode, result.data).accessToken
-                Log.d("token", token)
+        authVkLauncher = VK.login(this) { result : VKAuthenticationResult ->
+            when (result) {
+                is VKAuthenticationResult.Success -> {
+                    Log.d("auth",VK.getUserId().toString())
+                    Log.d("auth", result.token.accessToken)
+                }
+                is VKAuthenticationResult.Failed -> {
+                    // User didn't pass authorization
+                }
             }
         }
 
@@ -35,15 +41,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-        navView.setOnItemSelectedListener(){
+        navView.setOnItemSelectedListener{
             when (it.itemId)
             {
                 R.id.home -> supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment_activity_main, HomeFragment()).commit()
                 R.id.map -> supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment_activity_main, MapFragment()).commit()
-                R.id.settings-> supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment_activity_main, SettingsFragment()).commit()
+                R.id.settings -> supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment_activity_main, SettingsFragment()).commit()
             }
             return@setOnItemSelectedListener true
         }
         navView.selectedItemId = R.id.home
+
+        WorkManager.getInstance(applicationContext)
+                   .enqueue( OneTimeWorkRequest.Builder(VkWorker::class.java)
+                                                .setInitialDelay(10, TimeUnit.SECONDS)
+                                                .build())
     }
 }
+
