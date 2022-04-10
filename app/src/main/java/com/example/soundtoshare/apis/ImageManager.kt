7a59084a855430ba02f7eaabf50ba3edd532ba6e
Dmitry <dvm101@yandex.ru>
2022-04-10 03:10:15 +0300

    Bitmap в отдельном потоке

--- app/src/main/java/com/example/soundtoshare/apis/ImageManager.kt
+++ app/src/main/java/com/example/soundtoshare/apis/ImageManager.kt
@@ -1,72 +1,29 @@
-package com.example.soundtoshare
+package com.example.soundtoshare.apis
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.os.Handler
-import android.os.Message
-import android.util.Log
 import android.widget.ImageView
-import java.io.BufferedInputStream
+import kotlinx.coroutines.*
 import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
 import java.net.URL
 
-
 object ImageManager {
-    private const val TAG = "ImageManager"
-    fun fetchImage(iUrl: String?, iView: ImageView?) {
-        if (iUrl == null || iView == null) return
-        val handler: Handler = object : Handler() {
-            override fun handleMessage(message: Message) {
-                val image = message.obj as Bitmap
-                iView.setImageBitmap(image)
-            }
+
+    fun fetchImage(url: URL, iView: ImageView) {
+        val result: Deferred<Bitmap?> = GlobalScope.async {
+            url.toBitmap()
         }
-        val thread: Thread = object : Thread() {
-            override fun run() {
-                val image = downloadImage(iUrl)
-                if (image != null) {
-                    Log.v(TAG, "Got image by URL: $iUrl")
-                    val message = handler.obtainMessage(1, image)
-                    handler.sendMessage(message)
-                }
-            }
+
+        GlobalScope.launch(Dispatchers.Main) {
+            iView.setImageBitmap(result.await())
         }
-        thread.priority = 3
-        thread.start()
     }
 
-    fun downloadImage(iUrl: String): Bitmap? {
-        var bitmap: Bitmap? = null
-        var conn: HttpURLConnection? = null
-        var buf_stream: BufferedInputStream? = null
-        try {
-            Log.v(TAG, "Starting loading image by URL: $iUrl")
-            conn = URL(iUrl).openConnection() as HttpURLConnection
-            conn.doInput = true
-            conn!!.setRequestProperty("Connection", "Keep-Alive")
-            conn.connect()
-            buf_stream = BufferedInputStream(conn.inputStream, 8192)
-            bitmap = BitmapFactory.decodeStream(buf_stream)
-            buf_stream.close()
-            conn.disconnect()
-            buf_stream = null
-            conn = null
-        } catch (ex: MalformedURLException) {
-            Log.e(TAG, "Url parsing was failed: $iUrl")
-        } catch (ex: IOException) {
-            Log.d(TAG, "$iUrl does not exists")
-        } catch (e: OutOfMemoryError) {
-            Log.w(TAG, "Out of memory!!!")
-            return null
-        } finally {
-            if (buf_stream != null) try {
-                buf_stream.close()
-            } catch (ex: IOException) {
-            }
-            conn?.disconnect()
+    private fun URL.toBitmap(): Bitmap?{
+        return try {
+            BitmapFactory.decodeStream(openStream())
+        }catch (e: IOException){
+            null
         }
-        return bitmap
     }
 }
--- app/src/main/java/com/example/soundtoshare/apis/VkAPI.kt
+++ app/src/main/java/com/example/soundtoshare/apis/VkAPI.kt
@@ -3,25 +3,15 @@ package com.example.soundtoshare.apis
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
-import com.example.soundtoshare.ImageManager
 import com.vk.api.sdk.VK
 import com.vk.api.sdk.VKApiCallback
-import com.vk.api.sdk.requests.VKRequest
 import com.vk.sdk.api.audio.dto.AudioAudio
-import com.vk.sdk.api.base.dto.BaseUserGroupFields
 import com.vk.sdk.api.status.StatusService
 import com.vk.sdk.api.status.dto.StatusStatus
 import com.vk.sdk.api.users.UsersService
 import com.vk.sdk.api.users.dto.UsersFields
 import com.vk.sdk.api.users.dto.UsersUserFull
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.json.JSONException
-import org.json.JSONObject
 import java.net.URL
-import javax.net.ssl.HttpsURLConnection
 
 object VkAPI {
     var audio: AudioAudio? = null
@@ -48,7 +38,7 @@ object VkAPI {
             override fun success(result: List<UsersUserFull>) {
                 firstName.text = result[0].firstName
                 lastName.text = result[0].lastName
-                ImageManager.fetchImage(result[0].photo200,iView)
+                result[0].photo200?.let { ImageManager.fetchImage(URL(it),iView) }
             }
 
             override fun fail(error: Exception) {
