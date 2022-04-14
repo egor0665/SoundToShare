912d8aa7c53c34dce931ff53a42ac95da3861e0a
Dmitry <dvm101@yandex.ru>
2022-04-14 21:06:37 +0300

    Изменен метод загрузки аватарки в классе Network.kt

--- app/src/main/java/com/example/soundtoshare/apis/Network.kt
+++ app/src/main/java/com/example/soundtoshare/apis/Network.kt
@@ -3,22 +3,14 @@ package com.example.soundtoshare.apis
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import kotlinx.coroutines.*
-import java.io.IOException
 import java.net.URL
 
 class Network {
 
     fun loadAvatar(iUrl: String, loadAvatarCallback: Bitmap.() -> Unit) {
-        val conn = URL(iUrl).openConnection()
+        val connection = URL(iUrl).openConnection()
         CoroutineScope(Dispatchers.IO).launch {
-            withContext(Dispatchers.IO) {
-                try {
-                    conn.doInput = true
-                    val bufStream = conn.inputStream
-                    loadAvatarCallback(BitmapFactory.decodeStream(bufStream))
-                } catch (ex: IOException) {
-                }
-            }
+            loadAvatarCallback(BitmapFactory.decodeStream(connection.inputStream))
         }
     }
 }
