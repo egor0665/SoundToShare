fccee3dae6d02f171b55eeb2bdc40176b054057d
HolyDoly123 <vladimirudakovhd@gmail.com>
2022-05-10 13:01:03 +0300

    Починка логики обновления/добавления маркера + добавлена анимация появления маркера

--- app/src/main/java/com/example/soundtoshare/fragments/map/UpdateMarkersUseCase.kt
+++ app/src/main/java/com/example/soundtoshare/fragments/map/UpdateMarkersUseCase.kt
@@ -2,6 +2,11 @@ package com.example.soundtoshare.fragments.map
 
 import android.location.Location
 import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
+import android.view.animation.BounceInterpolator
+import android.view.animation.Interpolator
 import androidx.annotation.RequiresApi
 import com.example.soundtoshare.external.FirestoreDatabase
 import com.example.soundtoshare.repositories.User
@@ -10,6 +15,8 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlin.math.max
+
 
 class UpdateMarkersUseCase(vkAPIRepository: VkAPIRepository, private val fireStoreDatabase: FirestoreDatabase) {
     private lateinit var map: GoogleMap
@@ -21,13 +28,7 @@ class UpdateMarkersUseCase(vkAPIRepository: VkAPIRepository, private val fireSto
         fireStoreDatabase.notify.observeForever(){
             fireStoreDatabase.users.forEach() { user ->
                 if (user.VKAccount != myVkAccount && user.VKAccount != "null") {
-                    val userIndicator = MarkerOptions()
-                        .position(LatLng(user.geoPoint.latitude, user.geoPoint.longitude))
-                        .title(user.VKAccount)
-                        .snippet("lat:" + user.geoPoint.latitude + ", lng:" + user.geoPoint.longitude)
-                    val m = map.addMarker(userIndicator)
-                    m?.tag = user
-                    processMarker(m!!)
+                    processMarker(user)
                 }
             }
         }
@@ -44,18 +45,49 @@ class UpdateMarkersUseCase(vkAPIRepository: VkAPIRepository, private val fireSto
         fireStoreDatabase.fetchClosest(map.cameraPosition.target, results[0].toDouble())
     }
 
-    private fun processMarker(marker: Marker) {
-        val newUser: User = marker.tag as User
+    private fun processMarker(newUser: User) {
         val oldMarker = markers.find {
             val user: User = it.tag as User
             user.VKAccount == newUser.VKAccount
         }
         if (oldMarker != null) {
-            oldMarker.position = marker.position
+            oldMarker.position = LatLng(newUser.geoPoint.latitude, newUser.geoPoint.longitude)
         }
         else {
-            markers.add(marker)
+            val userIndicator = MarkerOptions()
+                .position(LatLng(newUser.geoPoint.latitude, newUser.geoPoint.longitude))
+                .title(newUser.VKAccount)
+                .snippet("lat:" + newUser.geoPoint.latitude + ", lng:" + newUser.geoPoint.longitude)
+            val newMarker = map.addMarker(userIndicator)
+            dropPinEffect(newMarker!!)
+            newMarker.tag = newUser
+            markers.add(newMarker)
         }
+    }
+
+    private fun dropPinEffect(marker: Marker) {
+        val handler = Handler(Looper.getMainLooper())
+        val start = SystemClock.uptimeMillis()
+        val duration: Long = 1500
+        val interpolator: Interpolator = BounceInterpolator()
+        handler.post(object : Runnable {
+            override fun run() {
+                val elapsed = SystemClock.uptimeMillis() - start
+                val t = max(
+                    1 - interpolator.getInterpolation(
+                        elapsed.toFloat()
+                                / duration
+                    ), 0f
+                ).toFloat()
+                marker.setAnchor(0.5f, 1.0f + 14 * t)
+                if (t > 0.0) {
+                    // Post again 15ms later.
+                    handler.postDelayed(this, 15)
+                } else {
+                    marker.showInfoWindow()
+                }
+            }
+        })
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
