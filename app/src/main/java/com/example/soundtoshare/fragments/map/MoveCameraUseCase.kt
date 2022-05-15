39d202f96c43ca36734be506ed4adf80a2b0e612
egor0665 <galancc@mail.ru>
2022-05-15 22:05:42 +0300

    Reactions V0.0

--- app/src/main/java/com/example/soundtoshare/fragments/map/MoveCameraUseCase.kt
+++ app/src/main/java/com/example/soundtoshare/fragments/map/MoveCameraUseCase.kt
@@ -1,6 +1,5 @@
 package com.example.soundtoshare.fragments.map
 
-import com.example.soundtoshare.repositories.LocationModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
