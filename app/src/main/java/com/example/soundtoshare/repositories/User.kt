ec1feae7db6e766e2760671f6cfe259020f6c1a5
egor0665 <galancc@mail.ru>
2022-05-21 22:57:54 +0300

    test data need to merge

--- app/src/main/java/com/example/soundtoshare/repositories/User.kt
+++ app/src/main/java/com/example/soundtoshare/repositories/User.kt
@@ -4,4 +4,4 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.GeoPoint
 
 
-data class User(val geoPoint: GeoPoint, val VKAccount: String, val lastUpdate: Timestamp, val song: String = "", val artist: String = "", val VKId: String = "")
+data class User(val geoPoint: GeoPoint, val VKAccount: String, val lastUpdate: Long, val VKAccountID : String)
